f1d5fd9066a85936bb75e5f1516f2dac27999713
Sai Barath <saibarathdeep@outlook.com>
2023-06-30 17:21:15 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import StartUpPage from './Components/StartUpPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Watch Together
-        </p>
-      </header>
+      <StartUpPage />
     </div>
   );
 }
---
+++ src/Components/StartUpPage.jsx
@@ -0,0 +1,10 @@
+import IntroSlide from "./Home/IntroSlide";
+import '../Components/startUpPage.css';
+
+export default function StartUpPage() {
+    return (
+        <div className="startup-page-container">
+            <IntroSlide />
+        </div>
+    )
+}
